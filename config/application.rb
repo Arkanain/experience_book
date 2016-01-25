f397fbeb14b73f0af134a021637820d3468b15b2
Arkanain <arkanainKiller@gmail.com>
2016-01-25 22:14:13 +0200

    changes for assets

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,8 @@ module ExperienceBook
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.serve_static_assets = true
+
     config.autoload_paths += Dir["#{Rails.root}"]
   end
 end
