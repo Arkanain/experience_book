259fd99fbe63b07270a63f172da23552836a7831
Arkanain <arkanainKiller@gmail.com>
2016-01-25 21:33:09 +0200

    change styles for menu

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(function() {
+  var article_height = $('#articles').height();
+
+  if($('#content').height() < article_height) {
+    $('#content').height(article_height + 20);
+  }
+});
