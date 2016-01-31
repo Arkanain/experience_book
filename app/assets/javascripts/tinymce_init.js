7a3387510bc91029c8f1b716d2a4dd743b43fe7e
Arkanain <arkanainKiller@gmail.com>
2016-01-31 03:37:58 +0200

    fix indent for prev line

--- app/assets/javascripts/tinymce_init.js
+++ app/assets/javascripts/tinymce_init.js
@@ -15,11 +15,20 @@ $(function () {
       ed.on('keydown', function (event) {
         // For backspace on indent field to prevent format troubles
         if(event.keyCode == 8) {
-          var element = $(ed.selection.getNode());
-          if(element.parents('p').attr('style').indexOf('padding-left') > 0) {
-            ed.execCommand('outdent');
-            event.preventDefault();
-            return false;
+          var selection = ed.selection;
+          var elem_parent = selection.getSelectedBlocks()[0];
+
+          if(selection.getSel().anchorOffset == 0 && elem_parent) {
+            // When we have indent, when user press backspace, remove indent first before go to prev line
+            if ($(elem_parent).css('padding-left') != '0px') {
+              ed.execCommand('outdent');
+              event.preventDefault();
+              return false;
+            }
+            // Don't break indent of prev line when we concat it with current
+            else {
+              $(elem_parent).css('padding-left', $(elem_parent).prev().css('padding-left'));
+            }
           }
         }
 
