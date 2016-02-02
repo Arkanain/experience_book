8945274b1c1973a545387653bcdff380fdeb93b6
Arkanain <arkanainKiller@gmail.com>
2016-02-02 03:15:03 +0200

    change functionality for tab, shift + tab, backspace

--- app/assets/javascripts/tinymce_init.js
+++ app/assets/javascripts/tinymce_init.js
@@ -5,6 +5,7 @@ $(function () {
     statusbar: false,
     plugins: ["textcolor", "paste", "save"],
     toolbar: "save | undo redo | forecolor backcolor | sizeselect | bold italic | fontselect | fontsizeselect",
+    invalid_elements : 'pre',
     paste_postprocess: function(plugin, args) {
       // Change pasted text font-style and font-family to default editor settings
       $(args.node).find('span').each(function(index, element) {
@@ -14,33 +15,22 @@ $(function () {
     },
     setup: function (ed) {
       ed.on('keydown', function (event) {
-        // For backspace on indent field to prevent format troubles
-        if(event.keyCode == 8) {
-          var selection = ed.selection;
-          var elem_parent = selection.getSelectedBlocks()[0];
+        var element = $(ed.selection.getStart());
+        var padding_left = parseInt(element.css('padding-left'), 10);
 
-          // If we are at the start of the string
-          if(selection.getSel().anchorOffset == 0) {
-            // If we have indent in current row remove it first before go to prev row
-            if ($(elem_parent).css('padding-left') != '0px') {
-              ed.execCommand('outdent');
-              event.preventDefault();
-              return false;
-            }
-            // If prev line has indent set it to current line and then concat current and prev row
-            else {
-              $(elem_parent).css('padding-left', $(elem_parent).prev().css('padding-left'));
-            }
-          }
+        // remove data attribute which are blocked changes for padding
+        element.removeAttr('data-mce-style');
+
+        // For backspace on indent field to prevent format troubles
+        if (event.keyCode == 8 && padding_left > 0) {
+          element.css('padding-left', padding_left - 30);
+          event.preventDefault();
+          return false;
         }
 
-        // For tab and shift + tab
+        // For tab(increased padding-left) and shift + tab(decreased padding-left)
         if(event.keyCode == 9) {
-          if (event.shiftKey) {
-            ed.execCommand('outdent');
-          } else {
-            ed.execCommand('indent');
-          }
+          element.css('padding-left', padding_left + (event.shiftKey ? -30 : 30));
           event.preventDefault();
           return false;
         }
@@ -48,3 +38,8 @@ $(function () {
     }
   });
 });
+
+//$('iframe').contents().find('p').each(function(i, e) {
+//  $(e).css('padding-left', 0);
+//  $(e).removeAttr('data-mce-style');
+//});
