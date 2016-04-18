4bb4c11a60108aeb2714680618e8d0c9b76d39d3
Arkanain <arkanainKiller@gmail.com>
2016-04-18 03:03:11 +0300

    basical fixes for edit

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,4 +1,13 @@
 $(function() {
+  setTimeout(function() {
+    // In new/edit page
+    if($('#articles iframe')[0]) {
+      changeSaveButton($('#article_title')[0]);
+
+      $('#articles iframe').height($('#content').height() - 180);
+    }
+  }, 1000);
+
   window.changeSaveButton = function(selector) {
     tinyMCE.activeEditor.theme.panel.find('toolbar *')[1].disabled(selector.value.trim() == '');
   };
